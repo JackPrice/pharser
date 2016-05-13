6a38b24b6aa25e8254aa5a65ad84a8ceb3e59705
Jack Price <jack@wearenifty.co.uk>
2016-05-13 18:21:36 +0100

    Create grammar exception structure

---
+++ src/Grammar/Exception/Exception.php
@@ -0,0 +1,11 @@
+<?php namespace Pharser\Grammar\Exception;
+
+/**
+ * A base exception for all grammar-related exceptions to extend.
+ *
+ * @author Jack Price <jackprice@outlook.com>
+ */
+class Exception extends \Exception
+{
+    //
+}
